c77142c0289836b8cb6c884ec6e9633b29a26eec
Candela Vulcano <candelavul@gmail.com>
2026-07-29 12:42:48 -0300

    Update producto.py

---
+++ src/models/producto.py
@@ -0,0 +1,12 @@
+﻿from sqlalchemy import Integer, String
+from sqlalchemy.orm import Mapped, mapped_column
+
+from .base import Base
+
+
+class Producto(Base):
+    __tablename__ = "productos"
+
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    nombre: Mapped[str] = mapped_column(String(100), nullable=False)
+    precio: Mapped[int] = mapped_column(Integer, nullable=False)
