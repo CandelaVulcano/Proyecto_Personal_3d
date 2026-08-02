6dd8d3a9c23d23d59c07af3b2b15737f0dda3a3e
Candela Vulcano <candelavul@gmail.com>
2026-08-02 18:20:53 -0300

    test

--- tests/unit/test_producto.py
+++ tests/unit/test_producto.py
@@ -10,11 +10,11 @@ def test_registrar_producto_con_sesion_de_test(db_session):
         nombre="Producto 1",
         descripcion="Descripción del producto 1",
         precio=100.0,
-        stock=10,
+        
     )
 
     assert producto.nombre == "Producto 1"
     assert producto.descripcion == "Descripción del producto 1"
     assert producto.precio == 100.0
-    assert producto.stock == 10
+
 
